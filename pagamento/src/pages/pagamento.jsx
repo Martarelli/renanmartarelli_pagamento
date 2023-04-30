8c4dd5991d7df7b985a47aa21f9e8cbe752524b7
Martarelli <renanmartarelli@gmail.com>
2023-04-30 19:45:49 -0300

    configuração inicial pagina pagamento

---
+++ pagamento/src/pages/pagamento.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const pages = () => {
+  return <View style={{flex:1, backgroundColor: '#7FC7BD'}}>
+
+  </View>;
+}
+
+export default pages;
